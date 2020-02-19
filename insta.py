11024a4d3167a9010e540a9bcd4b312b561386b4
namita2310 <namitashetty01@gmail.com>
2020-02-19 10:13:22 +0530

    first commit

---
+++ insta.py
@@ -0,0 +1,48 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
+import re
+from time import sleep, strftime
+# from random import randint
+import pandas as pd
+
+chromedriver_path = "/home/dell/geckodriver" # Change this to your own chromedriver path!
+# webdriver = webdriver.Chrome(executable_path=chromedriver_path)
+sleep(2)
+driver = webdriver.Firefox(executable_path=chromedriver_path)
+
+driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+sleep(3)
+
+
+username = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')
+username.send_keys('codeitout')
+password = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')
+password.send_keys('Ganesh@21')
+
+button_login = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]')
+button_login.click()
+sleep(5)
+notnow=driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]')
+notnow.click()
+sleep(5)
+
+driver.get("https://www.instagram.com/explore/tags/programming/")
+sleep(3)
+
+img1=driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]")
+img1.click()
+sleep(5)
+like=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]")
+like.click()
+sleep(5)
+
+for i in range(0,10):
+    next_butt=driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a")
+    next_butt.click()
+    sleep(5)
+    like=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]")
+    like.click()
+    sleep(5)
