0d80507af314012429986ec3344b7a3c92f1d537
namita2310 <namitashetty01@gmail.com>
2020-02-19 23:37:08 +0530

    30 pics

--- insta.py
+++ insta.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 import re
 from time import sleep, strftime
+import random
 # from random import randint
 import pandas as pd
 
@@ -48,10 +49,35 @@ sleep(3)
 
 
 
-for i in range(0,100):
+for i in range(0,29):
+    x=random.randrange(1,10)
     next_butt=driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a[2]")
     next_butt.click()
-    sleep(3)
+    sleep(x)
     like=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]")
     like.click()
-    sleep(3)
+    print(i)
+    sleep(x)
+
+img2=driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div/div[9]/div[1]/a/div[1]/div[2]")
+img2.click()
+sleep(5)
+like2=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button/svg")
+like2.click()
+sleep(5)
+next_butt2=driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a[2]")
+next_butt2.click()
+sleep(3)
+like2=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button/svg")
+like2.click()
+sleep(3)
+
+for i in range(0,29):
+    x=random.randrange(1,10)
+    next_butt2=driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a[2]")
+    next_butt2.click()
+    sleep(x)
+    like2=driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button/svg")
+    like2.click()
+    print(i)
+    sleep(x)
